8ce96a4ac91f8949267915edd82a277275ceeb11
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-05-06 13:41:16 +0530

    "currencyConvertor"

---
+++ 06currencyConverter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/${currency}.json`
+    )
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]));
+    console.log("the first call:", data);
+  }, [currency]);
+
+  console.log("The data is :", data);
+  return data;
+}
+
+export default useCurrencyInfo;
